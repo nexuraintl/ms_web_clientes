808e9bd2e81d2a5197256de0f6254fa6feb43078
santiago_valenzuela <svalenzuela@nexura.com>
2026-02-25 14:12:10 -0500

    Despliegue del microservicio qa-clientes 25/02/2026

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b330888ed34794b6185108149e951f93.js"
+  "./precache-manifest.885c047a7ce2e73cd6ef5aa63e2cfeac.js"
 );
 
 self.addEventListener('message', (event) => {
