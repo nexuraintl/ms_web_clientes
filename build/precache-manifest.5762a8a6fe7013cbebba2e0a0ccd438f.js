eb83188473eb025cd78f03b54f1064b547c4c945
santiago_valenzuela <svalenzuela@nexura.com>
2026-03-02 14:45:45 -0500

    Despliegue del build en ambiente QA

---
+++ build/precache-manifest.5762a8a6fe7013cbebba2e0a0ccd438f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8018b72cdd55d171928ea192317dddec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56e1a5024aa8a77f290e",
+    "url": "./static/css/2.94d5dd74.chunk.css"
+  },
+  {
+    "revision": "951404aa9464c129662c",
+    "url": "./static/css/main.4ac2e74d.chunk.css"
+  },
+  {
+    "revision": "56e1a5024aa8a77f290e",
+    "url": "./static/js/2.e63e07f2.chunk.js"
+  },
+  {
+    "revision": "81a4050d951d5fafb106642f6b21b35b",
+    "url": "./static/js/2.e63e07f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951404aa9464c129662c",
+    "url": "./static/js/main.9613a8ea.chunk.js"
+  },
+  {
+    "revision": "acc926bb605940aaa5e6",
+    "url": "./static/js/runtime-main.cad7f980.js"
+  },
+  {
+    "revision": "b2ab2ed77756687a42afa2ff99f9f3aa",
+    "url": "./static/media/beep1sec.b2ab2ed7.mp3"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "./static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "02c308c85836603730f903c75c81f643",
+    "url": "./static/media/primeicons.02c308c8.eot"
+  },
+  {
+    "revision": "5caee686d05a0993fe09711e09c1389f",
+    "url": "./static/media/primeicons.5caee686.woff"
+  },
+  {
+    "revision": "aef2b0c4a5a1966adcbb0e07733f0493",
+    "url": "./static/media/primeicons.aef2b0c4.woff2"
+  },
+  {
+    "revision": "c55055c0c2514f8d890c9976111e9fdb",
+    "url": "./static/media/primeicons.c55055c0.svg"
+  },
+  {
+    "revision": "e88a3b64f676a48401f2aebfe2bc8fe2",
+    "url": "./static/media/primeicons.e88a3b64.ttf"
+  }
+]);
